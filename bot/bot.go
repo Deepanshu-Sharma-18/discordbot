73235c34eb0ecfcc60619f9d70ad9609038ad9e1
Deepanshu Sharma <deepanshu.r.sharma@gmail.com>
2023-02-27 22:32:21 +0530

    first commit

---
+++ bot/bot.go
@@ -0,0 +1,67 @@
+package bot
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/bwmarrin/discordgo"
+	"github.com/joho/godotenv"
+)
+
+type Jokes struct {
+	ID     string `json:"id"`
+	Joke   string `json:"joke"`
+	Status int    `json:"status"`
+}
+
+func Botops(s *discordgo.Session, m *discordgo.MessageCreate) {
+
+	if m.Author.ID == s.State.User.ID {
+		return
+	}
+	if m.Content == "!random" {
+		req, err := http.NewRequest("GET", "https://icanhazdadjoke.com/", nil)
+		if err != nil {
+			panic(err)
+		}
+		req.Header.Set("Accept", "text/plain")
+		client := &http.Client{}
+		response, err := client.Do(req)
+		if err != nil {
+			panic(err)
+		}
+		defer response.Body.Close()
+		body, _ := ioutil.ReadAll(response.Body)
+
+		s.ChannelMessageSend(m.ChannelID, string(body))
+	}
+
+}
+
+func Main() {
+	godotenv.Load(".env")
+	sess, err := discordgo.New("Bot " + os.Getenv("Token"))
+	if err != nil {
+		panic(err)
+	}
+
+	sess.AddHandler(Botops)
+
+	sess.Identify.Intents = discordgo.IntentsAllWithoutPrivileged
+	err = sess.Open()
+	if err != nil {
+		panic(err)
+	}
+	defer sess.Close()
+
+	fmt.Println("bot is running")
+
+	sc := make(chan os.Signal, 1)
+	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt)
+	<-sc
+
+}
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/Deepanshu-Sharma-18/discordbot/bot"
+)
+
+func main() {
+	bot.Main()
+}
